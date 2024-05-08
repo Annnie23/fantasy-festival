0cd668481ad76b84c10cf59beb84f881524d7efe
Annnie23 <143446906+Annnie23@users.noreply.github.com>
2024-05-08 21:55:03 +0200

    this and that

--- fantasy-festival/src/components/SiteNavigation.vue
+++ fantasy-festival/src/components/SiteNavigation.vue
@@ -68,21 +68,6 @@ export default {
 
 <style scoped>
 
-/* Reset some default styles for the page */
-/* body, html {
-    margin: 0;
-    padding: 0;
-    font-family: Arial, sans-serif;
-} */
-
-/* Styling for the header section */
-/* header {
-    background-color: #023a6b;
-    color: #f2b613;
-    padding: 20px 0;
-    z-index: 1000;
-} */
-
 
 .navbar {
     
@@ -113,30 +98,19 @@ span{
     padding-left: 10px;
 }
 
-/* .logo {
-    font-size: 24px;
-    font-weight: bold;
-} */
 
-/* .nav-links {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-} */
 
 .nav-links li {
     display: inline;
     margin-right: 15px;
 }
 
-/* .nav-links li a {
-    color: #f2b613;
-    text-decoration: none;
-    transition: color 0.3s ease;
-}
-
-.nav-links li a:hover {
-    color: #ffd700; /* Change to desired hover color */
-/* } */
+ @media screen and (max-width: 768px) {
+    .navbar-brand {
+        display: block;
+        text-align: center;
+    }
+    
+ }
 
 </style>
