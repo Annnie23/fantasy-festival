eff6007a799f0aa1df584e9dc2fcab4316816128
Annnie23 <143446906+Annnie23@users.noreply.github.com>
2024-05-29 12:27:57 +0200

    nav back

--- fantasy-festival/src/components/SiteNavigation.vue
+++ fantasy-festival/src/components/SiteNavigation.vue
@@ -1,27 +1,11 @@
 <template>
-    <link rel="stylesheet" href="../assets/main.css" type="text/css" charset="utf-8" />
   <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
       <div class="container-fluid">
         <RouterLink to="/" class="navbar-brand">
-          <img
-            src="../assets/img/logo.png"
-            alt="Logo"
-            width="150"
-            height="auto"
-            style="pointer-events: none"
-          />
+          <img src="../assets/img/logo.png" alt="Logo" width="150" height="auto" style="pointer-events: none;">
         </RouterLink>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          @click="toggleBurgerMenu"
-        >
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -29,50 +13,30 @@
           <ul class="navbar-nav">
             <RouterLink to="/" class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">
-                <img
-                  src="../assets/img/hjem.png"
-                  alt="Forside"
-                  width="auto"
-                  height="58"
-                />
+                <img src="../assets/img/hjem.png" alt="Forside" width="auto" height="58">
               </a>
               <span v-if="isScrolled">Forside</span>
             </RouterLink>
 
             <RouterLink to="/program" class="nav-item">
               <a class="nav-link" href="#">
-                <img
-                  src="../assets/img/program.png"
-                  alt="Program"
-                  width="auto"
-                  height="58"
-                />
+                <img src="../assets/img/program.png" alt="Program" width="auto" height="58">
               </a>
               <span v-if="isScrolled">Program</span>
             </RouterLink>
-
-            <RouterLink to="/quest" class="nav-item">
+          
+              <RouterLink to="/quest" class="nav-item">
               <a class="nav-link" href="#">
-                <img
-                  src="../assets/img/quest.png"
-                  alt="Quest"
-                  width="auto"
-                  height="56"
-                />
+                <img src="../assets/img/quest.png" alt="Quest" width="auto" height="56">
               </a>
               <span v-if="isScrolled">Quest</span>
             </RouterLink>
-
+            
             <li class="nav-item">
               <a class="nav-link" href="https://fantasyfestival.dk/">
-                <img
-                  src="../assets/img/drage.png"
-                  alt="Festival"
-                  width="auto"
-                  height="65"
-                />
-              </a>
-              <span v-if="isScrolled">Fantasy Festival</span>
+                <img src="../assets/img/drage.png" alt="Festival" width="auto" height="65">
+                </a>
+                <span v-if="isScrolled">Fantasy Festival</span>
             </li>
           </ul>
         </div>
@@ -85,51 +49,37 @@
 export default {
   data() {
     return {
-      isScrolled: false,
-    };
+      isScrolled: false
+    }
   },
   mounted() {
-    const navEl = document.querySelector(".navbar");
-    const navCollapseEl = document.querySelector(".navbar-collapse");
+    const navEl = document.querySelector('.navbar');
+    const navCollapseEl = document.querySelector('.navbar-collapse');
 
-    window.addEventListener("scroll", () => {
+    window.addEventListener('scroll', () => {
       if (window.scrollY >= 56) {
-        navEl.classList.add("navbar-scrolled");
+        navEl.classList.add('navbar-scrolled');
         this.isScrolled = true;
       } else if (window.scrollY < 56) {
-        navEl.classList.remove("navbar-scrolled");
+        navEl.classList.remove('navbar-scrolled');
         this.isScrolled = false;
       }
 
-      if (this.isScrolled && navCollapseEl.classList.contains("show")) {
+      if (this.isScrolled && navCollapseEl.classList.contains('show')) {
         anime({
-          targets: ".navbar-collapse",
-          translateY: "-100%",
+          targets: '.navbar-collapse',
+          translateY: '-100%',
           duration: 1000,
-          easing: "easeInOutQuad",
-          complete: function (anime) {
-            navCollapseEl.classList.remove("show");
+          easing: 'easeInOutQuad',
+          complete: function(anime) {
+            navCollapseEl.classList.remove('show');
             if (window.innerWidth <= 768) {
-              // Add this condition
-              navCollapseEl.style.transform = "translateY(0)"; // Set translateY to its initial value
+              navCollapseEl.style.transform = 'translateY(0)';
             }
-          },
+          }
         });
       }
     });
-
-    document.addEventListener("click", (event) => {
-      const burgerMenuButton = document.querySelector(".navbar-toggler");
-      if (!burgerMenuButton.contains(event.target) && !navCollapseEl.contains(event.target)) {
-        burgerMenuButton.classList.remove("active");
-      }
-    });
-  },
-  methods: {
-    toggleBurgerMenu() {
-      const burgerMenuButton = document.querySelector(".navbar-toggler");
-      burgerMenuButton.classList.toggle("active");
-    }
   }
 }
 </script>
@@ -143,52 +93,42 @@ export default {
     justify-content: space-between;
     align-items: center;
     transition: all 0.7s !important;
-    z-index: 5;
+    z-index: 1;
   
 }
 
-.navbar.navbar-scrolled {
-  background-color: var(--blåsort) !important;
-  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.15);
-  max-height: 135px;
+
+.navbar.navbar-scrolled{
+    background-color: var(--blåsort) !important;
+    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.15);
+
 }
 
 .navbar-brand {
-  margin-left: 20px;
-  transition: transform 0.7s; /* Add a transition for the transform property */
+    margin-left: 20px;
+    transition: transform 0.7s; /* Add a transition for the transform property */
 }
 
 .navbar.navbar-scrolled .navbar-brand {
   transform: scale(0.8); /* Reduce the size of the logo to 80% */
 }
 
-.navbar-nav {
-  margin-left: auto;
-}
-.navbar-collapse.show {
-  position: absolute; /* or 'fixed' */
-  top: 70%; /* position it right below the navbar */
-  right: 0; /* align it to the left */
-  z-index: 1000; /* adjust as needed to ensure it's on top of other elements */
-  border: 2px solid var(--orange);
-  background-color: var(--blåsort);
-  padding: 10px;
-  border-radius: 5px;
-  width: 50%; /* make it span the full width of the viewport */
-  color:var(--gul)
+.navbar-nav{
+    margin-left: auto;
+
 }
 
-span {
-  color: var(--gul);
-  font-size: large;
-  padding-left: 5px;
-  text-decoration: none;
+span{
+    color: var(--gul);
+    font-size: large;
+    padding-left: 5px;
+    text-decoration: none;
 }
 
 a {
   text-decoration: none !important;
-}
 
+}
 .nav-item {
   display: flex;
   flex-direction: row-reverse;
@@ -219,23 +159,30 @@ a {
   width: 100%;
 }
 
-@media screen and (max-width: 768px) {
-  .navbar-brand img {
-    justify-content: center;
-    display: flex;
-    width: 100px;
-  }
 
-  .navbar-collapse .nav-item span,
+
+
+
+@media screen and (max-width: 768px) {
+   
+    .navbar-brand img{
+        justify-content: center;
+        display: flex;
+        width: 100px;
+    }
+
+   
+    .navbar-collapse .nav-item span,
   .navbar-collapse .nav-item img {
     display: inline-block;
     transition: padding 0s !important; 
-    padding-left: 10px;
+    padding-left: 0;
     padding-top: 10px;
   }
-}
+    
+ } 
 
-@media (max-width: 767px) {
+ @media (max-width: 767px) {
   .nav-item span {
     margin-top: 0; /* Adjust this value as needed */
   }
@@ -246,7 +193,5 @@ a {
         font-size: medium;
     }
  }
-
- 
 
 </style>
