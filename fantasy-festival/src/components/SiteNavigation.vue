5ffb8a12df6c34d18698bf08da77dde9b72525fa
Louisebrorsen <loubro01@easv365.dk>
2024-05-29 15:25:06 +0200

    jaaaaa

--- fantasy-festival/src/components/SiteNavigation.vue
+++ fantasy-festival/src/components/SiteNavigation.vue
@@ -1,33 +1,39 @@
 <template>
   <header>
+    <!-- Navbar starter -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
       <div class="container-fluid">
+        <!-- Logo med RouterLink -->
         <RouterLink to="/" class="navbar-brand">
           <img src="../assets/img/logo.png" alt="Logo" width="150" height="auto" style="pointer-events: none;">
         </RouterLink>
+        <!-- Navbar-togglingsknap til mobil -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
+        <!-- Indholdet af den sammenklappelige navbar -->
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
+            <!-- Link til Program -->
             <RouterLink to="/program" class="nav-item program">
               <a class="nav-link" href="#">
                 <img src="../assets/img/program2.png" alt="Program" width="auto" height="58">
               </a>
-              <span v-if="isScrolled">Program</span>
+              <span v-if="isScrolled">Program</span> 
             </RouterLink>
-          
-              <RouterLink to="/quest" class="nav-item quest">
+            <!-- Link til Quest -->
+            <RouterLink to="/quest" class="nav-item quest">
               <a class="nav-link" href="#">
                 <img src="../assets/img/program.png" alt="Quest" width="auto" height="58">
               </a>
-              <span v-if="isScrolled">Quest</span>
+              <span v-if="isScrolled">Quest</span> 
             </RouterLink>
           </ul>
         </div>
       </div>
     </nav>
+    <!-- Navbar slutter -->
   </header>
 </template>
 
@@ -35,13 +41,14 @@
 export default {
   data() {
     return {
-      isScrolled: false
+      isScrolled: false 
     }
   },
   mounted() {
     const navEl = document.querySelector('.navbar');
     const navCollapseEl = document.querySelector('.navbar-collapse');
 
+    /* Eventlytter til scroll */
     window.addEventListener('scroll', () => {
       if (window.scrollY >= 56) {
         navEl.classList.add('navbar-scrolled');
@@ -51,6 +58,7 @@ export default {
         this.isScrolled = false;
       }
 
+    /*   Hvis navbar er scrollet og sammenklapningen vises */
       if (this.isScrolled && navCollapseEl.classList.contains('show')) {
         anime({
           targets: '.navbar-collapse',
@@ -71,7 +79,7 @@ export default {
 </script>
 
 <style scoped>
-
+/* Styling for navbar */
 .navbar { 
     background-color: transparent !important;
     margin: 0 auto;
@@ -82,38 +90,43 @@ export default {
     padding-top: 8px;
 }
 
-
-.navbar.navbar-scrolled{
+/* Styling for scrollet navbar */
+.navbar.navbar-scrolled {
     background-color: var(--blåsort) !important;
     box-shadow: 0 3px 10px rgba(0, 0, 0, 0.15);
     max-height: 135px;
 }
 
+/* Styling for navbar-brand */
 .navbar-brand {
     margin-left: 20px;
-    transition: transform 0.7s; /* Add a transition for the transform property */
+    transition: transform 0.7s; 
 }
 
+/* Styling for scrollet navbar-brand */
 .navbar.navbar-scrolled .navbar-brand {
-  transform: scale(0.8); /* Reduce the size of the logo to 80% */
+  transform: scale(0.8); 
 }
 
-.navbar-nav{
+/* Styling for navbar-navigation */
+.navbar-nav {
     margin-left: auto;
-
 }
 
-span{
+/* Styling for links */
+span {
     color: var(--gul);
     font-size: large;
     text-decoration: none;
 }
 
+/* Styling for links */
 a {
   text-decoration: none !important;
   padding-top: 0;
-
 }
+
+/* Styling for navbar-items */
 .nav-item {
   display: flex;
   flex-direction: row-reverse;
@@ -125,74 +138,21 @@ a {
   z-index: 50;
 }
 
+/* Styling for hover-effekt på navbar-items */
 .nav-item:hover {
-  color: var(--gul); /* Ændrer farven til gul ved hover */
-  transform: scale(1.1) !important; /* Skalerer elementet op ved hover */
+  color: var(--gul); 
+  transform: scale(1.1) !important; 
 }
 
-
-
+/* Styling for tekst på navbar-items */
 .nav-item span {
   opacity: 1;
   transition: opacity 0.7s, transform 0.7s;
   margin-top: -10px;
 }
 
+/* Styling for tekst på navbar-items ved hover */
 .nav-item:hover span {
   opacity: 0.7;
 }
-
-.navbar-toggler {
-  background-color: var(--orange);
-}
-
-.navbar-collapse {
-  background-color: transparent;
-  position: relative;
-  padding-right: 10px;
-  width: 100%;
-}
-
-
-
-
-
-@media screen and (max-width: 768px) {
-   
-    .navbar-brand img{
-        justify-content: center;
-        display: flex;
-        width: 100px;
-    }
-
-   
-    .navbar-collapse .nav-item span,
-  .navbar-collapse .nav-item img {
-    display: inline-block;
-    transition: padding 0s !important; 
-    padding-left: 10px;
-    padding-top: 10px;
-  }
-
-}
-
- @media (max-width: 767px) {
-  .nav-item span {
-    margin-top: 0; /* Adjust this value as needed */
-  }
-}
-
- @media screen and (max-width: 1100px){
-    .nav-item span {
-        font-size: medium;
-    }
- }
- .navbar-collapse .nav-item span,
-  .navbar-collapse .nav-item img {
-    display: inline-block;
-    transition: padding 0s !important; 
-    padding-left: 10px;
-    padding-top: 10px;
-  }
-
 </style>
--- fantasy-festival/src/views/HomeView.vue
+++ fantasy-festival/src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <template>
+    <!-- Henvisning til fonttype -->
     <link rel="stylesheet" href="../assets/main.css" type="text/css" charset="utf-8" />
 
+    <!-- Karousel sektion -->
   <div id="demo" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
+        <!-- Billede -->
         <img src="../assets/img/slide1.jpg" alt="slide" class="d-block w-100" />
         <div class="carousel-caption d-none d-md-block">
+          <!-- Tekstboks - bred skærm -->
           <div class="text-box-left">
             <h3>Velkommen til Fantasy Quest Esbjerg</h3>
             <p>
@@ -19,6 +23,7 @@
           </div>
         </div>
         <div class="carousel-caption d-block d-md-none">
+          <!-- Tekstboks - smal skærm -->
           <div class="text-box-left-small">
             <h3>Velkommen til Fantasy Quest Esbjerg</h3>
             <p>Her på siden finder du alle relevante informationer om årets Quest.
@@ -31,8 +36,10 @@
       </div>
 
       <div class="carousel-item">
+        <!-- Billede -->
         <img src="../assets/img/vk.jpg" alt="slide" class="d-block w-100" />
         <div class="carousel-caption d-none d-md-block">
+          <!-- Tekstboks - bred skærm -->
           <div class="text-box-left">
             <h3>Mød de fire kæmper</h3>
             <p>
@@ -44,6 +51,7 @@
           </div>
         </div>
       <div class="carousel-caption d-block d-md-none">
+          <!-- Tekstboks - smal skærm -->
           <div class="text-box-left-small">
             <h3>Mød de fire kæmper</h3>
             <p>Kæmperne er fire store, stærke og modige helte, som er udvalgt til at redde verden fra de onde magter.
@@ -55,8 +63,10 @@
       </div>
 
       <div class="carousel-item">
+        <!-- Billede -->
         <img src="../assets/img/lakse-karousel.png" alt="slide" class="d-block w-100" />
         <div class="carousel-caption d-none d-md-block">
+          <!-- Tekstboks - bred skærm -->
           <div class="text-box-left">
             <h3>Mød Lakserytteren</h3>
             <p>
@@ -68,6 +78,7 @@
           </div>
         </div>
       <div class="carousel-caption d-block d-md-none">
+          <!-- Tekstboks - smal skærm -->
           <div class="text-box-left-small">
             <h3>Mød Lakserytteren</h3>
             <p>Lakserytteren har i år været med til at skabe Fantasy Quest Esbjerg.
@@ -79,8 +90,10 @@
       </div>
 
       <div class="carousel-item">
+      <!-- Billede -->
         <img src="../assets/img/slide4.jpg" alt="slide" class="d-block w-100" />
         <div class="carousel-caption d-none d-md-block">
+          <!-- Tekstboks - bred skærm -->
           <div class="text-box-left">
             <h3>Et magisk univers</h3>
             <p>
@@ -92,6 +105,7 @@
           </div>
         </div>
       <div class="carousel-caption d-block d-md-none">
+          <!-- Tekstboks - smal skærm -->
           <div class="text-box-left-small">
             <h3>Et magisk univers</h3>
             <p>Du vil blive ført ind i et magisk univers, hvor monstre og mystiske væsner bor.
@@ -103,8 +117,10 @@
       </div>
 
       <div class="carousel-item">
+        <!-- Billede -->
         <img src="../assets/img/aften.jpg" alt="slide" class="d-block w-100" />
         <div class="carousel-caption d-none d-md-block">
+          <!-- Tekstboks - bred skærm -->
           <div class="text-box-left">
             <h3>Onde magter vil herske</h3>
             <p>
@@ -116,6 +132,7 @@
           </div>
         </div>
       <div class="carousel-caption d-block d-md-none">
+          <!-- Tekstboks - smal skærm -->
           <div class="text-box-left-small">
             <h3>Onde magter vil herske</h3>
             <p>Hjælp med at redde verden fra de onde magter.
@@ -128,8 +145,10 @@
       </div>
 
       <div class="carousel-item">
+        <!-- Billede -->
         <img src="../assets/img/slide6.jpg" alt="slide" class="d-block w-100" />
         <div class="carousel-caption d-none d-md-block">
+          <!-- Tekstboks - bred skærm -->
           <div class="text-box-left">
             <h3>Lade musikken guide dig</h3>
             <p>
@@ -141,6 +160,7 @@
           </div>
         </div>
       <div class="carousel-caption d-block d-md-none">
+          <!-- Tekstboks - smal skærm -->
           <div class="text-box-left-small">
             <h3>Lade musikken guide dig</h3>
             <p>Du vil møde syngene og spilende væsner, som vil guide dig gennem Questen.
@@ -152,8 +172,10 @@
       </div>
 
       <div class="carousel-item">
+        <!-- Billede -->
         <img src="../assets/img/slide7.jpg" alt="slide" class="d-block w-100" />
         <div class="carousel-caption d-none d-md-block">
+          <!-- Tekstboks - bred skærm -->
           <div class="text-box-left">
             <h3>Årets præmier</h3>
             <p>
@@ -165,6 +187,7 @@
           </div>
         </div>
       <div class="carousel-caption d-block d-md-none">
+          <!-- Tekstboks - smal skærm -->
           <div class="text-box-left-small">
             <h3>Årets præmier</h3>
             <p>Der vil være præmier til de bedste helte, som har klaret Questen.
@@ -177,8 +200,10 @@
       </div>
 
       <div class="carousel-item">
+        <!-- Billede -->
         <img src="../assets/img/slide3.jpg" alt="slide" class="d-block w-100" />
         <div class="carousel-caption d-none d-md-block">
+          <!-- Tekstboks - bred skærm -->
           <div class="text-box-left">
             <h3>Slip fantasien løs</h3>
             <p>
@@ -192,6 +217,7 @@
         </div>
       
       <div class="carousel-caption d-block d-md-none">
+          <!-- Tekstboks - smal skærm -->
           <div class="text-box-left-small">
             <h3>Slip fantasien løs</h3>
             <p>Vi hjælper dig med at slippe fantasien løs og leve dig ind i en anden verden.
@@ -204,6 +230,7 @@
       </div>
     </div>
 
+    <!-- Område til at skifte billede -->
     <button
       class="carousel-control-prev"
       type="button"
@@ -222,13 +249,17 @@
     </button>
   </div>
 
+  <!-- Sektion med de 3 cards på forsiden -->
   <section class="bg-green">
     <div class="container py-6">
       <div class="row justify-content-center">
+        <!-- Første kort -->
         <div class="col-md-4">
           <div class="card-home">
+            <!-- billede til kort -->
               <img src="../assets/img/drage.png" alt="" width="auto" height="70">
             <router-link to="/program">
+              <!-- tekst til kort -->
             <div class="card-body">
               <h3 class="text-2xl font-bold">Fantasy Festival</h3>
               <p>
@@ -241,11 +272,14 @@
           </router-link>
           </div>
         </div>
+        <!-- Andet kort -->
         <div class="col-md-4">
           <div class="card-home">
+            <!-- billede til kort -->
               <img src="../assets/img/program.png" alt="" width="auto" height="70">
             <router-link to="/quest">
             <div class="card-body">
+              <!-- Tekst til kort -->
               <h3 class="text-2xl font-bold">Fantasy Quest</h3>
               <p>
                 Er du klar til at leve dig ind i en anden verden, en magisk
@@ -257,12 +291,15 @@
           </router-link>
           </div>
         </div>
+        <!-- Tredje kort -->
         <div class="col-md-4">
           <div class="card-home">
+            <!-- billede til kort -->
             <a href="https://www.ticketmaster.dk/artist/fantasyfestival-billetter/1233494">
               <img src="../assets/img/quest.png" alt="" width="auto" height="70">
             </a>
             <div class="card-body">
+              <!-- Tekst til kort -->
               <h3 class="text-2xl font-bold">Billetter</h3>
               <p>
                 Der er gratis adgang flere forskellige steder, men aktiviteter
@@ -279,6 +316,10 @@
 </template>
 
 <script>
+/* Denne kode er en animation på kortne på forsiden.
+Den gør at de flyver ind fra venstre og sætter sig.
+Animationen bliver kun afspillet en gang, når kortene er synlige på skærmen.
+*/
 export default {
   mounted() {
     let animationPlayed = false;
@@ -325,7 +366,7 @@ export default {
   position: relative;
 }
 
-
+/* Stilen for overlayet på billederne */
 .carousel-item::before {
   content: "";
   position: absolute;
@@ -336,26 +377,30 @@ export default {
   background: linear-gradient(to right, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
 }
 
+/* Stilen for billederne i karousellen */
 .carousel-item img {
   width: 100%;
   height: 100vh;
   object-fit: cover;
 }
 
+/* Stilen for teksten i karousellen */
 .carousel-item.active .carousel-caption {
-  opacity: 1; /* Show the caption on the active slide */
+  opacity: 1; 
 }
 
+/* Stilen for tekstboksen i karousellen */
 .carousel-caption {
   position: sticky;
   bottom: 30%;
   left: 100px;
   display: inline-block;
-  width: 90%; /* Tilføj bredde for bedre justering på små skærme */
+  width: 90%; 
   max-width: 500px;
   text-align: left;
 }
 
+/* Stilen for tekstboksen i karousellen på små skærme */
 .text-box-left {
   background: linear-gradient(270deg, rgba(80,180,165,1) 0%, rgba(14,35,66,0.8576680672268908) 0%);
   color: var(--gul);
@@ -376,6 +421,7 @@ export default {
   line-height: 1.5;
 }
 
+/* Stilen for tekstboksen i karousellen på små skærme */
 .text-box-left-small {
   background: linear-gradient(270deg, rgba(80,180,165,1) 0%, rgba(14,35,66,0.8576680672268908) 0%);
   color: var(--gul);
@@ -383,34 +429,39 @@ export default {
   border-radius: 5px;
 }
 
+/* Stilen for overskriften i tekstboksen */
 .text-box-left-small {
   font-size: 14px;
 }
 
+/* Stilen for teksten i tekstboksen */
 .text-box-left-small h3 {
   font-size: 18px;
 }
 
+/* Stilen for teksten i tekstboksen */
 .text-box-left-small p {
   font-size: 12px;
 }
 
+/* Stilen for områderne til at skifte billede */
 .carousel-control-prev,
 .carousel-control-next {
   opacity: 0;
 }
 
+/* Stilen for områderne til at skifte billede ved hover */
 .carousel:hover .carousel-control-prev,
 .carousel:hover .carousel-control-next {
   opacity: 0;
 }
 
 /* Stilen for de 3 cards på forsiden */
-
 .bg-green {
 margin-top: 1rem;
 }
 
+/* Stilen for kortne */
 .card-home {
   margin-bottom: 1rem;
   margin-left: 1rem;
@@ -427,12 +478,14 @@ margin-top: 1rem;
   cursor: pointer;
 }
 
+/* stilen for link */
 a:-webkit-any-link {
     color: var(--blåsort);
     cursor: pointer;
     text-decoration: none;
 }
 
+/* stilen for link ved hover */
 a:-webkit-any-link:hover {
     color: var(--gul);
     cursor: pointer;
@@ -455,7 +508,7 @@ a:-webkit-any-link:hover {
 }
 
 
-
+/* Stil på kort ved skærmbrede på max 768px */
 @media (max-width: 768px){
   .card-home{
     background: linear-gradient(270deg, rgba(80,180,165,1) 0%, rgba(14,35,66,0.8576680672268908) 0%);
@@ -464,12 +517,12 @@ a:-webkit-any-link:hover {
   }
 }
 
-/* Justering af karouselltekster for mindre skærme */
+/* Justering af karouselltekster for skærmbrede på max 980px */
 @media (max-width: 980px) {
   .carousel-caption {
     
     bottom: 10%;
-    left: 20px; /* Juster for at passe bedre på små skærme */
+    left: 20px; 
     width: 90%;
   }
   
@@ -492,6 +545,7 @@ a:-webkit-any-link:hover {
 }
 }
 
+/* Justering af karouselltekster for skærmbrede på max 768px */
 @media (max-width: 768px) {
   .carousel-caption {
     position: absolute;
